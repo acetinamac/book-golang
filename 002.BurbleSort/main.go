bab18f89b70eaf03cfae5f9b484186ee2846af0e
Armin Cetina Mac <armin.cetina.mac@outlook.com>
2026-01-01 14:47:28 -0500

    bubblesort

---
+++ 002.BurbleSort/main.go
@@ -0,0 +1,24 @@
+package main
+
+type Ordered interface {
+	~float64 | ~int | ~string
+}
+
+func bubbleSort[T Ordered](data []T) {
+	n := len(data)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if data[j] > data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
+			}
+		}
+	}
+}
+
+func main() {
+	// Example usage
+	ints := []int{64, 34, 25, 12, 22, 11, 90}
+	bubbleSort(ints)
+	println("Sorted integers:", ints)
+
+}
